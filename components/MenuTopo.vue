1208cc43f2d8150f1ca0138152420deffd93dcd3
Roberto Reis <tekinforroberto@gmail.com>
2022-05-28 22:07:40 -0300

    Instalação do font awesome

--- components/MenuTopo.vue
+++ components/MenuTopo.vue
@@ -2,24 +2,22 @@
   <div>
     <div class="bg_menu">
       <b-navbar toggleable="lg" type="dark" class="container xxl">
-        <b-button v-b-toggle.sidebar-backdrop>[icon]</b-button>
+
+        <b-button v-b-toggle.sidebar-backdrop>
+          <fa icon="bars" class="icon_menu" />
+        </b-button>
 
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
+        <div class="ml-auto">
+          <b-dropdown variant="link" toggle-class="text-decoration-none dropdown_user" no-caret right>
             <template #button-content>
-              <em>User [avatar]</em>
+              <em>User</em>
+              <fa icon="circle-user" class="circle_user" />
             </template>
-            <b-dropdown-item href="#">
-              Profile
-            </b-dropdown-item>
-            <b-dropdown-item href="#">
-              Sign Out
-            </b-dropdown-item>
-          </b-nav-item-dropdown>
-
-        </b-navbar-nav>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-dropdown>
+        </div>
       </b-navbar>
     </div>
 
@@ -34,6 +32,20 @@ export default {
 <style>
   .bg_menu {
     background-color: #455A64;
+  }
+  .icon_menu {
+    font-size: 1.5rem;
+    vertical-align: middle;
+  }
+  .circle_user {
+    font-size: 1.8rem;
+    vertical-align: middle;
+  }
+  .dropdown_user {
+    color: #fff;
+  }
+  .dropdown_user:hover {
+    color: #d1d1d1;
   }
 
 </style>
