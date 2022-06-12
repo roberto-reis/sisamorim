25f2dce00df2714acf41bca76074be5042af92ad
Roberto Reis <tekinforroberto@gmail.com>
2022-06-11 21:57:01 -0300

    SIS-6-feat: Criado a tela de Produtos

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -13,24 +13,51 @@
       </div>
       <nav class="mb-3 menu_sidenav">
         <b-nav vertical>
+          <li class="nav-item">
+            <nuxt-link to="/" class="nav-link">Dashboard</nuxt-link>
+          </li>
           <b-nav-item href="#">Clentes</b-nav-item>
           <b-nav-item href="#">Fornecedores</b-nav-item>
-          <b-nav-item href="#">Produtos</b-nav-item>
+          <li class="nav-item">
+            <nuxt-link to="/produto" class="nav-link">Produtos</nuxt-link>
+          </li>
           <b-nav-item href="#">Vendas</b-nav-item>
           <b-nav-item href="#">Serviços</b-nav-item>
           <b-nav-item href="#">Orçamentos</b-nav-item>
-          <b-nav-item href="#">Finaceiro</b-nav-item>
 
-          <b-nav-item v-b-toggle.collapse-admin href="#">
-            <div class="d-flex justify-content-between">
-              <span>Admin</span>
-              <fa icon="angle-down" class="text-center" />
+          <b-nav-item v-b-toggle.collapse-financeiro href="#">
+            <div class="d-flex justify-content-between align-items-center">
+              <span>Financeiro</span>
+              <fa icon="angle-down" />
             </div>
           </b-nav-item>
-          <b-collapse id="collapse-admin" class="mt-1 mx-1">
-            <b-card>
-              <a>Configuração</a>
-            </b-card>
+          <b-collapse id="collapse-financeiro" class="collapse_financeiro py-2 px-2">
+            <div class="sub_menu">
+              <b-nav vertical>
+                <li class="nav-item">
+                  <nuxt-link to="/financeiro" class="nav-link">Gerenciar Financeiro</nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link to="/centro-custo" class="nav-link">Centro de Custo</nuxt-link>
+                </li>
+              </b-nav>
+            </div>
+          </b-collapse>
+
+          <b-nav-item v-b-toggle.collapse-admin href="#">
+            <div class="d-flex justify-content-between align-items-center">
+              <span>Admin</span>
+              <fa icon="angle-down" />
+            </div>
+          </b-nav-item>
+          <b-collapse id="collapse-admin" class="collapse_financeiro py-2 px-2">
+            <div class="sub_menu">
+              <b-nav vertical>
+                <li class="nav-item">
+                  <nuxt-link to="/configuracao" class="nav-link">Configuração</nuxt-link>
+                </li>
+              </b-nav>
+            </div>
           </b-collapse>
 
         </b-nav>
@@ -73,10 +100,23 @@
     border-radius: 4px;
     font-size: 30px;
   }
-    .b-sidebar-header .close svg:hover {
-      background-color: #D11300;
-      color: #fff;
-      border-color: #D11300;
-    }
+  .b-sidebar-header .close svg:hover {
+    background-color: #D11300;
+    color: #fff;
+    border-color: #D11300;
+  }
+  .collapse_financeiro {
+    border-bottom: 1px solid #DEDEDE;
+    background-color: #E6EBEC;
+  }
+  .sub_menu {
+    background-color: #fff;
+    border-radius: 0.25rem;
+    border: 1px solid #DEDEDE;
+  }
+  .sub_menu ul a {
+    color: #555;
+    font-weight: 500;
+  }
 
 </style>
---
+++ pages/produto/create.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>
+    Novo Produto
+  </h2>
+</template>
+
+<script>
+export default {
+  name: 'CreateProduto',
+  layout: 'default'
+}
+</script>
+
+<style>
+
+</style>
