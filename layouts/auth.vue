d7e8cda4ce29c5aa7a6743dc319d26a994c620ef
Roberto Reis <tekinforroberto@gmail.com>
2022-07-24 10:21:41 -0300

    SIS-42-chore: Criado a tela de login

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-info">
+  <div>
     <div class="container">
       <div class="row">
-        <div class="col-md-6 mt-4">
+        <div class="col-md-12">
           <Nuxt />
         </div>
       </div>
